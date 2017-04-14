f96a640efd66781dfac841a79762a50d7e0250d5
Codyme <codyme.git@gmail.com>
2017-04-14 21:21:52 +1000

    adds user repos section

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -9,11 +9,18 @@ import { GithubService } from '../../services/github.service';
 export class ProfileComponent implements OnInit {
 
   user: any[];
+  repos: any[];
 
   constructor(private _githubService: GithubService) { 
+
     this._githubService.getUser().subscribe( user => {
       this.user = user;
     });
+
+    this._githubService.getRepos().subscribe( repos => {
+      this.repos = repos;
+    });
+
   }
 
   ngOnInit() {
